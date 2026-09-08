0037a4c4b1ca60b43db82ebb8b0cdd45d3ba9638
Ken M. Haggerty <khaggerty@decisionpointcorp.com>
2026-09-08 10:19:54 -0700

    Adjust Paper styling through theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,22 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
+const increment = 16;
+
 const theme = createTheme({
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          boxSizing: "border-box",
+          padding: increment,
+        },
+      },
+    },
+  },
+  shape: {
+    borderRadius: increment,
+  },
+  spacing: increment,
   typography: {
     h1: {
       fontSize: "2.0rem",
