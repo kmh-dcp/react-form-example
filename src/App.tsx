e99702bd08b37042c2920edf8f0c55f9bc3f5744
Ken M. Haggerty <khaggerty@decisionpointcorp.com>
2026-09-08 10:20:14 -0700

    Left-align header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { Paper, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
+  const sxPaper = {
+    textAlign: "left",
+  };
+
   return (
-    <Paper aria-label="paper">
-      <Typography aria-label="header" variant="h1">
+    <Paper aria-label="paper" sx={sxPaper}>
+      <Typography aria-label="header" variant="h1" className="hugContents">
         React Form Example
       </Typography>
     </Paper>
