f5f3d29b3903c925886c8f7a8a8e33972a47fd21
Ken M. Haggerty <khaggerty@decisionpointcorp.com>
2026-09-08 20:06:37 -0700

    Update ReactFormExample with internal state set with initial values via props

--- src/ReactFormExample.tsx
+++ src/ReactFormExample.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+import type {
+  ChangeEventHandler,
+  Dispatch,
+  FocusEventHandler,
+  SetStateAction,
+} from "react";
 import { Stack, TextField } from "@mui/material";
 
-function ReactFormExample() {
+export type FormExampleData = {
+  firstName?: string;
+  lastName?: string;
+};
+
+function ReactFormExample({ ...formFields }: FormExampleData) {
+  const [firstName, setFirstName] = useState(formFields.firstName ?? "");
+  const [lastName, setLastName] = useState(formFields.lastName ?? "");
+
+  const onChange = (
+    setter: Dispatch<SetStateAction<string>>,
+  ): ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> => {
+    return (event) => {
+      setter(event.target.value);
+    };
+  };
+
+  const onBlur:
+    | FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>
+    | undefined = () => {
+    // TODO: propogate values back to parent component
+  };
+
   const sxFirstName = {
     width: 300,
   };
@@ -11,8 +40,21 @@ function ReactFormExample() {
 
   return (
     <Stack direction="row">
-      <TextField label="First name" sx={sxFirstName} />
-      <TextField label="Last name" sx={sxLastName} required />
+      <TextField
+        label="First name"
+        value={firstName}
+        sx={sxFirstName}
+        onChange={onChange(setFirstName)}
+        onBlur={onBlur}
+      />
+      <TextField
+        label="Last name"
+        value={lastName}
+        sx={sxLastName}
+        onChange={onChange(setLastName)}
+        onBlur={onBlur}
+        required
+      />
     </Stack>
   );
 }
