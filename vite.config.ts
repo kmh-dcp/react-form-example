3b6858e091503ae4efaf078848d857e6b76fc7cc
Ken M. Haggerty <khaggerty@decisionpointcorp.com>
2026-09-10 19:42:58 -0700

    Combine vitest.config.ts into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text"],
+    },
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/vitest.setup.ts",
+  },
 });
