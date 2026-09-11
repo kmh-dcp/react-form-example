8a48af7825b83c833425582f4bb34368b61fbd77
Ken M. Haggerty <khaggerty@decisionpointcorp.com>
2026-09-10 20:19:20 -0700

    Update App.tsx to improve testability

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,66 @@
 import { render, screen, within } from "@testing-library/react";
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 import App from "./App.tsx";
 
 describe("App", () => {
+  it("renders", () => {
+    render(<App aria-label="FOO" />);
+
+    const component = screen.getByLabelText("FOO");
+
+    expect(component).toBeInTheDocument();
+  });
+
   it("contains Paper component", () => {
-    render(<App />);
-    const paper = screen.getByLabelText("paper");
+    render(<App aria-label="FOO" />);
+
+    const component = screen.getByLabelText("FOO");
+    const paper = within(component).getByLabelText("paper");
 
     expect(paper).toBeInTheDocument();
   });
 
   it("displays header", () => {
-    render(<App />);
-    const paper = screen.getByLabelText("paper");
+    render(<App aria-label="FOO" />);
+
+    const component = screen.getByLabelText("FOO");
+    const paper = within(component).getByLabelText("paper");
     const header = within(paper).getByLabelText("header");
 
     expect(header).toHaveTextContent("React Form Example");
+  });
+
+  // TODO: Adjust once ThemeProvider is refactored out into parent component
+  describe.skip("theme", () => {
+    // it('defaults to light theme', () => {
+    //   const theme = useTheme();
+    //
+    //   expect(theme.palette.mode).toBe('light');
+    // });
+
+    describe("can be set to dark theme", () => {
+      // it('via prop', () => {
+      //   render(<App darkTheme />);
+      //   const theme = useTheme();
+      //
+      //   expect(theme.palette.mode).toBe('dark');
+      // });
+
+      it("via toggle", async () => {
+        const callback = vi.fn();
+        const user = userEvent.setup();
+
+        render(<App aria-label="FOO" themeDidChange={callback} />);
+
+        const component = screen.getByLabelText("FOO");
+        const toggle = within(component).getByLabelText("theme-switch");
+
+        await user.click(toggle);
+
+        expect(callback).toHaveBeenCalledTimes(1);
+        expect(callback).toHaveBeenCalledWith("dark");
+      });
+    });
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -18,17 +18,36 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { type StandardProps } from "./SharedUtils.ts";
 import "./App.css";
 
-function App({ "aria-label": ariaLabel }: StandardProps) {
-  const [theme, setTheme] = useState<Theme>(LightTheme);
+// TODO: Refactor ThemeProvider out into parent component for testability
+
+type AppProps = {
+  darkTheme?: boolean;
+  themeDidChange?: (theme: string) => void;
+  showFormWarning?: boolean;
+};
+
+function App({
+  themeDidChange,
+  "aria-label": ariaLabel,
+  ...props
+}: AppProps & StandardProps) {
+  const [theme, setTheme] = useState<Theme>(
+    props.darkTheme ? DarkTheme : LightTheme,
+  );
   const [formData, setFormData] = useState<FormExampleData>({});
   const [formErrors, setFormErrors] = useState<Record<string, string>>({});
-  const [showFormWarning, setShowFormWarning] = useState<boolean>(false);
+  const [showFormWarning, setShowFormWarning] = useState<boolean>(
+    props.showFormWarning ?? false,
+  );
 
   // Actions
 
   const switchDidToggle = (event: ChangeEvent<HTMLInputElement>) => {
     const toggle = event.target.checked;
     setTheme(toggle ? LightTheme : DarkTheme);
+    if (themeDidChange) {
+      themeDidChange(toggle ? "light" : "dark");
+    }
   };
 
   const didChangeValue = (key: keyof FormExampleData, value: string) => {
@@ -71,7 +90,12 @@ function App({ "aria-label": ariaLabel }: StandardProps) {
       <CssBaseline />
       <Stack aria-label={ariaLabel} sx={sxViewport}>
         <Box>
-          <Switch onChange={switchDidToggle} {...switchProps} defaultChecked />
+          <Switch
+            aria-label="theme-switch"
+            onChange={switchDidToggle}
+            {...switchProps}
+            defaultChecked
+          />
         </Box>
         <Paper aria-label="paper" sx={sxPaper}>
           <Stack direction="row" sx={{ justifyContent: "space-between" }}>
